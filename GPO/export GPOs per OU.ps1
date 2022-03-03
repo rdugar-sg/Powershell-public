43c03b3a6cace66292f241ed9dc90bfeb6cc30b3
schindler.philipp@gmx.de <schindler.philipp@gmx.de>
2022-03-03 09:14:01 +0100

    first

---
+++ GPO/export GPOs per OU.ps1
@@ -0,0 +1,30 @@
+#this copies boot image drivers
+#by Philipp Schindler
+#
+
+#requirements: RSAT installed for using get-GPOReport
+
+
+#parameter
+$domain="example.domain.com"
+$DC="exampleDC"
+$OU = "OU=test,OU=one,DC=test,DC=com"
+
+
+#start
+
+Get-ADOrganizationalUnit $OU
+$LinkedGPOs = Get-ADOrganizationalUnit $OU | Select-object -ExpandProperty LinkedGroupPolicyObjects
+$LinkedGPOGUIDs = $LinkedGPOs | ForEach-object{$_.Substring(4,36)}
+$LinkedGPOGUIDs | ForEach-object {
+
+$gpo=Get-GPO -Guid $_
+
+write-host $gpo.DisplayName
+$path='C:\scripts\' + $gpo.DisplayName + '.xml'
+write-host $path
+
+Get-GPOReport -Name $gpo.DisplayName -Domain "emea.corpdir.net" -Server $dc -ReportType XML -Path $path -Verbose
+
+
+}
