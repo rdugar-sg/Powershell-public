0bdfedb414126cb4716322632f99cb646eeca181
Philinger1 <96050818+Philinger1@users.noreply.github.com>
2022-10-07 12:08:59 +0200

    Create startstopVMs.ps1

---
+++ VM/startstopVMs.ps1
@@ -0,0 +1,44 @@
+$Credential = Get-Credential -Credential "admin@M365x61941857.onmicrosoft.com"
+Connect-AzAccount -Credential $Credential
+
+$VMFile = "C:\tools\azure VM\TempAzureVMs.txt"
+$ReportFile = "C:\tools\azure VM\TempVMStartReport.CSV"
+
+#start
+Foreach ($ThisVMNow in Get-Content "$VMFile")
+{
+
+$Error.Clear()
+
+Start-AzVM -ResourceGroupName "cm1" -Name "$ThisVMNow" -nowait
+IF ($Error.Count -ne 0)
+{
+
+$STR = "Azure Vitual Machine was Started : " + $ThisVMNow
+Add-Content $ReportFile $STR
+}
+else
+{
+
+$STR = "ERROR: Failed to start Azure Virtual Machine : " + $ThisVMNow+","+$Error[0]
+Add-Content $ReportFile $STR
+}
+}
+
+#stop
+Foreach ($ThisVMNow in Get-Content "$VMFile")
+{
+
+$Error.Clear()
+Stop-azvm -ResourceGroupName "cm1" -Name "$ThisVMNow" -nowait
+IF ($Error.Count -ne 0)
+{
+$STR = "Azure Virtual Machine was stopped : " + $ThisVMNow
+Add-Content $ReportFile $STR
+}
+else
+{
+$STR = "ERROR: Failed to stop Azure Virtual Machine : " + $ThisVMNow+","+$Error[0]
+Add-Content $ReportFile $STR
+}
+}
